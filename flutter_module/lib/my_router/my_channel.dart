d2d0d868b61c7e570fdff83766972384e596639d
huangyuan <huangyuan@huangyuanlove.com>
2025-05-31 22:00:18 +0800

    my flutter module

--- flutter_module/lib/my_router/my_channel.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/services.dart';
-
-class MyChannel {
-  final channel = const MethodChannel('com.huangyuanlove.demo/method');
-
-  Future<T?> callNative<T>(String method, [dynamic params]) {
-    return channel.invokeMethod(method, params);
-  }
-}
